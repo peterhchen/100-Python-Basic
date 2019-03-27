8c723beaa53eded88e72183e400c61fc444d6155
peterhchen <peter.hp.chen@gmail.com>
2019-03-27 13:44:17 -0700

    Variable

---
+++ 01-10/ch03_Setup/03_Setup.py
@@ -0,0 +1,5 @@
+import sys
+import time
+print (sys.version)
+time.sleep (1)
+print ('Hello')
---
+++ 01-10/ch05_Var/05_PrintString.py
@@ -0,0 +1,20 @@
+
+y = 1
+x = 2
+print (x+3)
+name = 'youtube'
+print (name)
+print (name + ' rocks')
+
+print ('Print Test')
+# Mix single anbd double quote
+print ("We're going to store")
+print ('He said "Hi"')
+print ('We\'re going to store')
+# Concatenation
+print ('Hi, ' + 'there')
+print ('Hi', 5)
+# Convert
+print ('Hi ' + str(5))
+print (int ('8') + 5)
+print (float ('8.5') + 5)
