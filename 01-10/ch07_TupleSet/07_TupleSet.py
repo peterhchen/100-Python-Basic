0e5c460b8a6826ca6dcb612046eb45033d25b586
peterhchen <peter.hp.chen@gmail.com>
2019-03-27 14:56:28 -0700

    Tuple and Set

---
+++ 01-10/ch07_TupleSet/07_TupleSet.py
@@ -0,0 +1,18 @@
+tup = (21, 36, 14, 25)
+print (tup)
+#tup[1]=33
+# Error: cannot change
+
+# covnert the tuple into list
+list_values = list (tup)
+print (list_values)
+
+# convert list into tuple
+tup_values = tuple (list_values)
+print (tup_values)
+
+# Curely brack is for set
+s = { 3, 3, 1, 1, 2, 3, 3}
+print (s) # {1, 2, 3}
+s = { 98, 75, 14, 25, 63}
+print (s) #  { 98, 75, 14, 25, 63}
