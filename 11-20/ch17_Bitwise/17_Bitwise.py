9c93ae64f391ddd6bfd1cd94d125ffc68dcab03f
peterhchen <peter.hp.chen@gmail.com>
2019-04-02 13:51:19 -0700

    Bitwise operator

---
+++ 11-20/ch17_Bitwise/17_Bitwise.py
@@ -0,0 +1,17 @@
+print ('~12 = ', ~12)
+# 12 = 00001100
+# ~12 = 11110011  ===> -13
+# 2's complement = 1's complement + 1
+# -13 => 13 = 00001101
+# 1's complment of 13 = 11110010
+# 1's complement of 13 + 1 = 11110011 => This is -13
+
+# Bitwsie AND, OR
+print ('12 & 13 = ', 12 & 13)
+print ('12 | 13 = ', 12 | 13)
+print ('bin (25) = ', bin (25), ' bin (30) = ', bin (30))
+print ('25 | 30 = ', 25 & 30)
+# XOR
+print ('25 ^ 30 = ', 25 ^ 30)
+# <<
+print ('10 << 2 = ', 10 << 2)
