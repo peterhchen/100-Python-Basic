1e26853747691f737aca128e475e2db49c3ca7cf
peterhchen <peter.hp.chen@gmail.com>
2019-04-04 13:41:03 -0700

    for

---
+++ 21-30/ch23_for/23_for.py
@@ -0,0 +1,13 @@
+x = 'peter'
+for i in x:
+    print (i)
+
+x = ['Peter', 'irene', 'Jonathan']
+for i in x:
+    print (i)
+# increment from 5 to < 10
+for i in range (5, 10, 1):
+    print (i)
+# decrement from 10 to > 5.
+for i in range (10, 5, -1):
+    print (i)
