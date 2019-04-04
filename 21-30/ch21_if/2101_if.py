742d0c5ead7c15ec6218e7833a09e6edbee73f34
peterhchen <peter.hp.chen@gmail.com>
2019-04-04 12:08:36 -0700

    if .. elif .. else

---
+++ 21-30/ch21_if/2101_if.py
@@ -0,0 +1,12 @@
+x = 2
+
+if (x == 1):
+    print ('one')
+elif (x == 2):
+    print ('two')
+elif (x == 3):
+    print ('three')
+elif (x == 4):
+    print ('four')
+else:
+    print ("Wrong Input")
---
+++ 21-30/ch21_if/21_if.py
@@ -0,0 +1,12 @@
+# if False
+if False:
+    print ('If block')
+
+print ('outside block')
+
+x = 8
+r = x % 2
+if r == 0:
+    print ('Even')
+else:
+    print ('Odd')
