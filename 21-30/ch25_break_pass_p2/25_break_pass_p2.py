3ef7316a37638cc3d0c417d7d159b5951408d0b8
peterhchen <peter.hp.chen@gmail.com>
2019-04-04 16:48:10 -0700

    break/pass part 2

---
+++ 21-30/ch25_break_pass_p2/25_break_pass_p2.py
@@ -0,0 +1,8 @@
+for i in range (10):
+    if (i == 3):
+        continue
+    if (i == 4):
+        pass # do not know what to implement yet. just pass. Nothing happen.
+    if (i == 5):
+        break
+    print ("Hello", i)
