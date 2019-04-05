99ffd3a3799e73705674ca8128d46399a6113fba
peterhchen <peter.hp.chen@gmail.com>
2019-04-04 21:00:52 -0700

    Print pattern

---
+++ 21-30/ch26_print_pattern/26_print_pattern.py
@@ -0,0 +1,27 @@
+# * * * *
+# * * * *
+# * * * *
+# * * * *
+for j in range (4):
+    for i in range(4):
+        print ('* ', end="")
+    print()
+print()
+# * 
+# * * 
+# * * * 
+# * * * *
+for j in range (4):
+    for i in range(j + 1):
+        print ('* ', end="")
+    print()
+print()
+# * * * * 
+# * * *
+# * *
+# * 
+for j in range (4):
+    for i in range(4 - j):
+        print ('* ', end="")
+    print()
+print()
