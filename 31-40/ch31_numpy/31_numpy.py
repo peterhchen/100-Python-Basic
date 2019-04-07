bc37d09f773abcdb3138871848d5335dfcdf156c
peterhchen <peter.hp.chen@gmail.com>
2019-04-06 21:59:36 -0700

    numpy

---
+++ 31-40/ch31_numpy/31_numpy.py
@@ -0,0 +1,9 @@
+#from array import *
+# array take at most 2 arguments
+#arr = array ('i', [1, 2, 3], [4, 5, 6])
+#print (arr)
+
+# numpy for single dimension array
+from numpy import *
+arr = array ([1, 2, 3, 4, 5, 6])
+print (arr)
